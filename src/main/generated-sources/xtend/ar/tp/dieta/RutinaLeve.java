e9e4a8d37f9bb4cf1a1b666bd4d603ad6d999cd7
daniel <yo_arkantos@hotmail.com>
2015-04-14 12:27:42 -0300

    TP1 Toja Daniel Arreglo error de mi commit

--- src/main/generated-sources/xtend/ar/tp/dieta/RutinaLeve.java
+++
@@ -1,7 +0,0 @@
-package ar.tp.dieta;
-
-import ar.tp.dieta.Rutina;
-
-@SuppressWarnings("all")
-public class RutinaLeve extends Rutina {
-}
--- src/main/generated-sources/xtend/ar/tp/dieta/RutinaMediana.java
+++
@@ -1,7 +0,0 @@
-package ar.tp.dieta;
-
-import ar.tp.dieta.Rutina;
-
-@SuppressWarnings("all")
-public class RutinaMediana extends Rutina {
-}
